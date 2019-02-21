26736bd41f56ddedbe6457be87134b38184aa68a
migmac99 <migmacthegamer@gmail.com>
2019-02-21 19:11:52 +0000

    stuff

---
+++ Unity/Golden Liquor/Assets/Scripts/Loading.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Loading : MonoBehaviour {
+
+    public void LoadLevel (int sceneIndex) {
+
+    }
+
+    IEnumerator LoadAsynchronously (int sceneIndex) {
+        
+
+    }
+}
