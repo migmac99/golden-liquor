80e192cc1685ce3e97c47c799574c0ce23a72b93
migmac99 <migmacthegamer@gmail.com>
2019-02-23 20:42:50 +0000

    transparency finalized + hitbox for trigger of transparency updated

--- Unity/Golden Liquor/Assets/Scripts/PlayerNavigation.cs
+++ Unity/Golden Liquor/Assets/Scripts/PlayerNavigation.cs
@@ -11,7 +11,8 @@ public class PlayerNavigation : MonoBehaviour {
 
     [Range (0, 1f)] public float ObjAlpha;
 
-    public GameObject TransparentObject;
+    public GameObject CameraToMouse_Object;
+    public GameObject PlayerToCamera_Object;
 
     private NavMeshAgent _navMeshAgent;
 
@@ -20,39 +21,71 @@ public class PlayerNavigation : MonoBehaviour {
     }
 
     void Update () {
+        #region CameraToMouseRay Camera sends raycast to mouse and checks for buildings in between + Player moves where clicked
 
-        Ray MyRay = Camera.main.ScreenPointToRay (Input.mousePosition);
-        RaycastHit HitInfo;
+        Ray CameraToMouseRay = Camera.main.ScreenPointToRay (Input.mousePosition);
+        RaycastHit CameraToMouseHitInfo;
 
         if (Input.GetMouseButtonDown (0)) { //if mouse clicked then go to mouse position
-            if (Physics.Raycast (MyRay, out HitInfo, 1000, WhatCanBeClickedOn)) {
-                _navMeshAgent.SetDestination (HitInfo.point);
+            if (Physics.Raycast (CameraToMouseRay, out CameraToMouseHitInfo, 1000, WhatCanBeClickedOn)) {
+                _navMeshAgent.SetDestination (CameraToMouseHitInfo.point);
             }
         }
 
-        if (Physics.Raycast (MyRay, out HitInfo, 1000, BuildingTag)) { //if mouse on top of building make transparent
-            Renderer[] renderers = HitInfo.transform.gameObject.GetComponentsInChildren<Renderer> ();
+        if (Physics.Raycast (CameraToMouseRay, out CameraToMouseHitInfo, 1000, BuildingTag)) { //if mouse on top of building make transparent
+            Renderer[] renderers = CameraToMouseHitInfo.transform.gameObject.GetComponentsInChildren<Renderer> ();
             foreach (var r in renderers) {
                 if (r.material.color.a == 1) {
                     r.material.color = new Color (1, 1, 1, ObjAlpha);
-                    TransparentObject = HitInfo.transform.gameObject;
+                    CameraToMouse_Object = CameraToMouseHitInfo.transform.gameObject;
                 }
             }
         }
 
-        if (TransparentObject != null) { //avoid null exception error
-            if (Physics.Raycast (MyRay, out HitInfo, 1000)) { //If mouse not on building make transparency 1
-                if (HitInfo.transform.name != TransparentObject.name) {
-                    Renderer[] renderers = TransparentObject.gameObject.GetComponentsInChildren<Renderer> ();
+        if (CameraToMouse_Object != null) { //avoid null exception error
+            if (Physics.Raycast (CameraToMouseRay, out CameraToMouseHitInfo, 1000)) { //If mouse not on building make transparency 1
+                if (CameraToMouseHitInfo.transform.name != CameraToMouse_Object.name) {
+                    Renderer[] renderers = CameraToMouse_Object.gameObject.GetComponentsInChildren<Renderer> ();
                     foreach (var r in renderers) {
                         if (r.material.color.a != 1) {
                             r.material.color = new Color (1, 1, 1, 1);
-                            TransparentObject = null;
+                            CameraToMouse_Object = null;
                         }
                     }
                 }
             }
         }
+        Debug.DrawRay (CameraToMouseRay.origin, CameraToMouseRay.direction * 1000, Color.red);
+        #endregion
+        #region PlayerToCameraRay Player sends raycast to camera and checks for buildings in between
 
+        Ray PlayerToCameraRay = Camera.main.ScreenPointToRay (transform.position);
+        RaycastHit PlayerToCameraHitInfo;
+
+        if (Physics.Raycast (PlayerToCameraRay.origin, transform.position - PlayerToCameraRay.origin, out PlayerToCameraHitInfo, 1000, BuildingTag)) {
+            Renderer[] renderers = PlayerToCameraHitInfo.transform.gameObject.GetComponentsInChildren<Renderer> ();
+            foreach (var r in renderers) {
+                if (r.material.color.a == 1) {
+                    r.material.color = new Color (1, 1, 1, ObjAlpha);
+                    PlayerToCamera_Object = PlayerToCameraHitInfo.transform.gameObject;
+                }
+            }
+        }
+
+        if (PlayerToCamera_Object != null) { //avoid null exception error
+            if (Physics.Raycast (PlayerToCameraRay.origin, transform.position - PlayerToCameraRay.origin, out CameraToMouseHitInfo, 1000)) { //If mouse not on building make transparency 1
+                if (CameraToMouseHitInfo.transform.name != PlayerToCamera_Object.name) {
+                    Renderer[] renderers = PlayerToCamera_Object.gameObject.GetComponentsInChildren<Renderer> ();
+                    foreach (var r in renderers) {
+                        if (r.material.color.a != 1) {
+                            r.material.color = new Color (1, 1, 1, 1);
+                            PlayerToCamera_Object = null;
+                        }
+                    }
+                }
+            }
+        }
+        Debug.DrawRay (PlayerToCameraRay.origin, transform.position - PlayerToCameraRay.origin, Color.red);
+        #endregion
     }
 }
